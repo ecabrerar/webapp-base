aa4c5da149cd090bcf62481c351fd9091ee91a40
Eudris Cabrera Rodriguez <eudris@gmail.com>
2014-01-09 19:23:26 -0500

    adding configuration and bootstrapping files

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+
+require_once ('web' . DIRECTORY_SEPARATOR . 'index.php');
+
