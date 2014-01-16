2d8cd9cb111abf2b317af2c840b7138913d502db
Eudris Cabrera Rodriguez <ecabrerar@users.noreply.github.com>
2014-01-16 00:33:03 -0500

    Update bootstrap.php

--- library/bootstrap.php
+++ library/bootstrap.php
@@ -3,4 +3,3 @@
 require_once (ROOT . DS . 'config' . DS . 'config.php');
 require_once (ROOT . DS . 'config' . DS . 'database.php');
 require_once (ROOT . DS . 'library' . DS . 'shared.php');
-require_once (ROOT . DS . 'library' . DS . 'shared.php');
