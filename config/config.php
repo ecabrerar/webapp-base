3b374d76987c228b0bbb6bc4db03dd616fd45996
Eudris Cabrera Rodriguez <eudris@gmail.com>
2014-01-16 00:22:40 -0500

    separate database configuration from main configuration file

--- config/config.php
+++ config/config.php
@@ -4,12 +4,6 @@
 
 define ('DEVELOPMENT_ENVIRONMENT',true);
 
-
-define('DB_NAME', 'Database Name');
-define('DB_USER', 'Database User');
-define('DB_PASSWORD', 'User Password');
-define('DB_HOST', 'Hostname');
-
 define('BASE_PATH','Base Path');
 
 
--- library/bootstrap.php
+++ library/bootstrap.php
@@ -1,4 +1,6 @@
 <?php
 //All needed files to start the application
 require_once (ROOT . DS . 'config' . DS . 'config.php');
+require_once (ROOT . DS . 'config' . DS . 'database.php');
+require_once (ROOT . DS . 'library' . DS . 'shared.php');
 require_once (ROOT . DS . 'library' . DS . 'shared.php');
