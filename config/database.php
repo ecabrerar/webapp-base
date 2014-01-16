e5a0bdd803e328baeea35a4a1e9d9b2c3e034784
Eudris Cabrera Rodriguez <eudris@gmail.com>
2014-01-16 00:23:55 -0500

    new database configuration file

---
+++ config/database.php
@@ -0,0 +1,8 @@
+<?php
+
+/** database configuration **/
+
+define('DB_NAME', 'Database Name');
+define('DB_USER', 'Database User');
+define('DB_PASSWORD', 'User Password');
+define('DB_HOST', 'Hostname');
