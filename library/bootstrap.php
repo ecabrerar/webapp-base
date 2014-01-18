41a9e38e9de13ef8d3aeb1d19b91b46920f8c1d9
Eudris Cabrera Rodriguez <eudris@gmail.com>
2014-01-18 09:12:59 -0500

    updating bootstrap.php

--- library/bootstrap.php
+++ library/bootstrap.php
@@ -1,6 +1,11 @@
 <?php
+
+
+if (!isset($_SERVER['HTTP_HOST'])) {
+    exit('This script cannot be run from the CLI. Run it from a browser.');
+}
+
 //All needed files to start the application
 require_once (ROOT . DS . 'config' . DS . 'config.php');
 require_once (ROOT . DS . 'config' . DS . 'database.php');
-require_once (ROOT . DS . 'library' . DS . 'shared.php');
 require_once (ROOT . DS . 'library' . DS . 'shared.php');
