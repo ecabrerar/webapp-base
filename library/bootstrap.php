c0ed1f84fc80f5dae07f6ec02acb35996dd76e6d
Eudris Cabrera Rodriguez <eudris@gmail.com>
2014-01-18 09:18:10 -0500

    updating bootstrap.php

--- library/bootstrap.php
+++ library/bootstrap.php
@@ -8,8 +8,5 @@ if (!isset($_SERVER['HTTP_HOST'])) {
 //All needed files to start the application
 require_once (ROOT . DS . 'config' . DS . 'config.php');
 require_once (ROOT . DS . 'config' . DS . 'database.php');
-<<<<<<< HEAD
 require_once (ROOT . DS . 'library' . DS . 'shared.php');
-=======
-require_once (ROOT . DS . 'library' . DS . 'shared.php');
->>>>>>> 2d8cd9cb111abf2b317af2c840b7138913d502db
+
